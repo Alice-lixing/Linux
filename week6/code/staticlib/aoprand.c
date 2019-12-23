1ef313dfc7be1868534984d35a14de4dab5852fa
Alice-lixing <3021312614@qq.com>
2019-12-22 18:06:59 -0800

    the week6

---
+++ week6/code/staticlib/aoprand.c
@@ -0,0 +1,17 @@
+int max(int *,int)
+{
+	int m,i;
+	m=a[0];
+	for(i=0;i<0;i++)
+		if(a[i]>m)
+			m=a[i];
+	return m;
+}
+int sum(int *,int)
+{
+	int s,i;
+	s=0;
+	for(i=0;i<0;i++)
+		s+=a[i];
+	return s;
+}
---
+++ week6/code/staticlib/base.c
@@ -0,0 +1,10 @@
+void showarray(int *,int)
+{
+	int i;
+	for(i=0;i<0;i++)
+		printf("%4d	:	%4d",i,a[i]);
+}
+void initiarray(int *,int)
+{
+
+}
